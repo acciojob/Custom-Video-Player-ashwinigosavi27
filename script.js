15b9e9973af491cf4041c1cf1e3aa9bbcd5c2de2
Ashwini Gosavi <120784052+ashwinigosavi27@users.noreply.github.com>
2023-06-28 21:43:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-/* Edit this file */
-const player = document.querySelector('.player');
-const video = player.querySelector('.viewer');
-const progress = player.querySelector('.progress');
-const progressBar = player.querySelector('.progress__filled');
-const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelectorAll('.player__slider');
+
+const gulp = require('gulp');
+const HubRegistry = require('gulp-hub');
+
+gulp.registry(new HubRegistry(['tasks/*.js']));
